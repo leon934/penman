1e902e0ae594e87f6eecf3a13232342aec8402e4
leon <leonlnguyen93@gmail.com>
2024-07-23 19:34:00 -0500

    Finished up algorithm for the four basic operators.
    Created another function for finding the numbers along with calculating the values after obtaining them.

--- math_algorithm.py
+++ math_algorithm.py
@@ -1,46 +1,51 @@
-
-
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def stringToEquation(stringArray: list) -> int:
-    value = ""
     numArray = []
-    operator = ""
 
-    for i in range(len(stringArray)):
-        string = stringArray.pop(0)
+    if len(stringArray) == 0:
+        raise Exception("No equation found.")
 
-        try:
-            int(string)
-            value += string
+    # Grabs the operator, and the first and second digits.
+    numArray.append(getNumber(stringArray))
+    operator = stringArray.pop(0)
+    numArray.append(getNumber(stringArray))
 
-        except(ValueError):
-            numArray.append(value)
-            value = ""
+    if numArray[1] == '':
+        raise Exception("Incomplete equation.")
 
-            if len(numArray) == 2: # Does the operation on two numbers once they exist.
-                match operator:
-                    case '+':
-                        print("+")
-                        numArray[0] = str(float(numArray[0]) + float(numArray[1]))
-                    case '-':
-                        print("-")
-                        numArray[0] = str(float(numArray[0]) - float(numArray[1]))
-                    case '*': # Figure this out later. (), *, \cdot, etc.
-                        numArray[0] = str(float(numArray[0]) * float(numArray[1]))
-                    case '/':
-                        numArray[0] = str(float(numArray[0]) / float(numArray[1]))
+    # Calculates the values given the numbers and operators.
+    while len(numArray) == 2:
+        match operator:
+            case '+':
+                numArray[0] = str(float(numArray[0]) + float(numArray[1]))
+            case '-':
+                numArray[0] = str(float(numArray[0]) - float(numArray[1]))
+            case '*': # FIXME: Figure this out later. (), *, \cdot, etc.
+                numArray[0] = str(float(numArray[0]) * float(numArray[1]))
+            case '/':
+                numArray[0] = str(float(numArray[0]) / float(numArray[1]))
 
-                del numArray[1]
+        del numArray[1]
 
-            operator = string
+        if len(stringArray) > 0: # If there's still values in the stringArray, continue calculating.
+            operator = stringArray.pop(0)
+            numArray.append(getNumber(stringArray))
     
+    return numArray[0]
 
-    print(numArray)
-    
-# def equationSolve(numberArray: list, operatorArray : list):
-#     print
-    
-    
-    
+# Finds the numbers up to the first operato or end.
+def getNumber(stringArray: list) -> int:
+    value = ""
 
-stringToEquation(['2', '3', '+', '3', '5', '-', '6'])
+    while len(stringArray) != 0 and stringArray[0] in numbers:
+        value += stringArray.pop(0)
+
+    return value
+
+# These are the test cases I used; feel free to try more.
+
+# print(stringToEquation([]))
+# print(stringToEquation(['2', '-']))
+# print(stringToEquation(['2', '3', '+', '3', '5']))
+# print(stringToEquation(['2', '3', '+', '3', '5', '-', '6']))
