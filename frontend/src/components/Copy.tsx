d62030ee68b50d023eb5b90781930dc5040133b0
leon <leonlnguyen93@gmail.com>
2025-06-22 14:31:20 -0500

    Restructured project files and removed deprecated folders.

---
+++ frontend/src/components/Copy.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useEffect } from 'react'
+import { useEditor } from 'tldraw'
+
+export const Copy = () => {
+    const editor = useEditor()
+
+    // FIXME: Remove this code after fully fleshing out creating output numbers.
+    useEffect(() => {
+        if(!editor) return
+
+        const onCopy = () => {
+            const selectedShape = editor.getSelectedShapes()[0]
+            
+            if(!selectedShape) return
+
+            const newShape = {
+                type: "draw",
+                x: selectedShape.x + 100,
+                y: selectedShape.y,
+                props: selectedShape.props
+            }
+
+            editor.createShape(newShape);
+
+            console.log(selectedShape);
+            
+            // const json = JSON.stringify(newShape, null, 4);
+            // const blob = new Blob([json], { type: "application/json" })
+            // const url = URL.createObjectURL(blob)
+
+            // const a = document.createElement('a')
+            // a.href = url
+
+            // const number = 1
+
+            // a.download = `${number}.json`
+            // a.click()
+
+            // URL.revokeObjectURL(url)
+        }
+
+        window.addEventListener('copy', onCopy);
+
+        return () => window.removeEventListener('copy', onCopy)
+    }, [editor])
+
+    return null
+}
